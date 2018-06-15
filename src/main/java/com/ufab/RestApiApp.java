96871b5b0de7aa13e779e927aa1cb44ab50b9093
SamuelJunior <smljr7@gmail.com>
2018-06-15 18:21:37 -0300

    Continuação do proj

--- src/main/java/com/ufab/RestApiApp.java
+++ src/main/java/com/ufab/RestApiApp.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication(scanBasePackages={"br.uepb.edu.ufab"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
-public class SpringBootRestApiApp {
+public class RestApiApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestApiApp.class, args);
+		SpringApplication.run(RestApiApp.class, args);
 	}
 }
